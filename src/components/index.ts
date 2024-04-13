5173275fa9b6ca81102305978deaf5fcd987744d
Newton Fernando de Souza Miranda Filho <newton.chickengopro@gmail.com>
2024-04-13 02:13:43 -0300

    feat: escravo do capitalismo

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export * from './button'
+export * from './input'
+export * from './label'
